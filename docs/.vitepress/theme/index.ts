0e4bfe7067346297e620471c3054ed7f8120ecd1
niechao136 <niechao136@users.noreply.github.com>
2025-06-04 03:26:59 +0000

    deploy: 916eee1dfb1cbd5e807f0f6ca20992f687cd30d6

--- docs/.vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import PostCard from './components/PostCard.vue'
-
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('PostCard', PostCard)
-  }
-}
